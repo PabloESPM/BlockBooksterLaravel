bbe9c31bb958ff63f48c0579cdacd7b9899c7243
Pablo <pabespmir@alu.edu.gva.es>
2026-01-18 16:52:36 +0100

    error en el factory fav list

--- database/factories/FavListFactory.php
+++ database/factories/FavListFactory.php
@@ -14,10 +14,13 @@ class FavListFactory extends Factory
 
     public function definition(): array
     {
+        // Define las opciones de visibilidad disponibles
+        $visibilities = ['private', 'public', 'friends'];
         return [
             'name' => $this->faker->sentence(3),      // Nombre de la lista
             'description' => $this->faker->paragraph(), // Descripción opcional
-            'description' => $this->faker->paragraph(), // Descripción opcional
+            // Usa faker->randomElement() para elegir uno aleatoriamente
+            'visibility' => $this->faker->randomElement($visibilities),
         ];
     }
 }
